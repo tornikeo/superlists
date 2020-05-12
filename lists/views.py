b41469a9e498bf70b84ecedce38f4b8fa63ef3dd
tornikeo <tonop15@freeuni.edu.ge>
2020-05-12 22:14:17 +0400

    input tag added into home.html

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'home.html')
+def home_page(request:HttpRequest,)->HttpResponse:
+    return render(request, 'home.html', {
+        'new_item_text':request.POST.get('item_text',''),
+    })
