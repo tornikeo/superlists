c8ccfb4e5878a9a8b65fa451cde665a967704bbd
tornikeo <tonop15@freeuni.edu.ge>
2020-05-14 11:12:23 +0400

    Model for lis Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -18,3 +19,21 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
     
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First Item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second Item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved = saved_items[0]
+        saved_second = saved_items[1]
+        self.assertEqual(first_saved.text, 'First Item')
+        self.assertEqual(saved_second.text, 'Second Item')
+
