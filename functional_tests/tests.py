1890a1d4a4700aad4804ce3ba4ba4ec0fca9ce40
tornikeo <tonop15@freeuni.edu.ge>
2020-05-24 13:00:02 +0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -110,4 +110,24 @@ class NewVisitorTest(LiveServerTestCase):
 
         # To Sleep
         
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
+        inputbox.send_keys("Testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
 
+        
