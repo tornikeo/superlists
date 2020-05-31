51cba424afd8b9f584790ee8a556595c3747ef80
tornikeo <tonop15@freeuni.edu.ge>
2020-05-31 12:53:23 +0400

    Moved FTs into their own inficidual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,42 +1,8 @@
 from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import os
+from .base import FunctionalTest
 
-MAX_WAIT = 5
-
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        options = webdriver.firefox.options.Options()
-        options.headless = True
-        self.browser = webdriver.Firefox(options=options)
-        staging_server = os.environ.get("STAGING_SERVER")
-        if staging_server:
-            self.live_server_url = "http://" + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(.5)
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-    
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -118,25 +84,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('milk', page_text)
 
         # To Sleep
-        
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10,
-        )
-        inputbox.send_keys("Testing")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Testing")
-        inputbox = self.browser.find_element_by_id("id_new_item")
-        self.assertAlmostEqual(
-            inputbox.location["x"] + inputbox.size['width'] / 2,
-            512,
-            delta=10,
-        )
-
         
