a04189a17682dbb36cd37c6e10e3b03b1ed43041
tornikeo <tonop15@freeuni.edu.ge>
2020-06-08 15:29:01 +0400

    Added missing import in list/forms.py

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -33,4 +34,3 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-            
