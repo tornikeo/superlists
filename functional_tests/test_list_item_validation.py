bc8b910918c2f28c4330f958a892e4db553dd969
tornikeo <tonop15@freeuni.edu.ge>
2020-06-09 10:23:19 +0400

    refactor FTs to use get_item_input_box

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to home page and accidentally tries to
         # submit an empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # The home page refreshes and there is an error message 
         # Saying that list items cannot be blank
@@ -20,12 +20,12 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # She tries again with some text for the item, which now works
-        self.browser.find_element_by_id('id_text').send_keys("Buy milk")
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely she now decides to submit a second blank list item
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.browser.find_elements_by_css_selector(
@@ -33,8 +33,8 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # And she can correct it by filling some text in
-        self.browser.find_element_by_id('id_text').send_keys("Make tea")
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Make tea")
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
@@ -42,15 +42,15 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_duplicate_items(self):
         # Edith goes to home page and starts a new list
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_text').send_keys("Buy milk")
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
         # The list is correctly diplayed
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # She tries entering a duplicate
-        self.browser.find_element_by_id('id_text').send_keys("Buy milk")
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
 
         # The home page refreshes and there is an error message 
@@ -59,3 +59,12 @@ class ItemValidationTest(FunctionalTest):
             self.browser.find_element_by_css_selector('.has-error').text,
             "You've already got this in your list",
         ))
+
+    def test_errors_are_cleared_on_input(self):
+        #Edith starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        # The list is correctly diplayed
+        self.wait_for_row_in_list_table('1: Buy milk')
